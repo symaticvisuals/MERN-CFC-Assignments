a2a02d867886e0167c14f098b3118de4e509f9ab
Deepanshu Goel <59753906+symaticvisuals@users.noreply.github.com>
2021-04-07 02:01:53 +0530

    Assignment-1 complete

---
+++ Assignment-1/patterns/pattern5.js
@@ -0,0 +1,21 @@
+function factorial(number) {
+    
+    let fact = 1;
+    while (number > 1) {
+        fact = fact * number;
+        number--;
+    }
+    return fact;
+}
+console.log(factorial(5,1));
+let n = 5;
+let row = 0;
+while (row < n) {
+    let col = 0;
+    while (col <= row) {
+        process.stdout.write(factorial(row) / (factorial(col) * factorial(row-col))+" ");
+        col++;
+    }
+    row++;
+    console.log();
+}
